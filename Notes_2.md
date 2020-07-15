7740bc54f3073eba32cccd8ecf47ab76953cff1e
Kenzy Louis <klouis@cognitivescale.com>
2020-07-15 07:01:16 -0500

    added notes_2 - initial mocking of templates

---
+++ Notes_2.md
@@ -0,0 +1,6 @@
+0. Prerequisites
+Module-1
+
+1. Mock user template
+- replace `Hello, world!` with a dictionary with `{username: USER_NAME}`
+- return an in line HTML
--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'kenzy'}
+    return """
+        <html>
+            <head>
+                <title>Home - My Blog</title>
+            </head>
+            <body>
+                <h2>Hello, """ + user['username'] + """!!</h2>
+            </body>
+        </html>
+    """
