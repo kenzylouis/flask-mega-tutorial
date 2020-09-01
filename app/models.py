6a9c2a2e5aea91d1a74c3f5195812ec8742dfdcd
Kenzy Louis <klouis@cognitivescale.com>
2020-09-01 13:18:14 -0500

    Corrected app/models.py to add UserMixin as parent to the User class

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
